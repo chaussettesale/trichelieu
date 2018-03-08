2b10e758939aedb33cc302feee49e75fe0c15845
chaussettesale <37185103+chaussettesale@users.noreply.github.com>
2018-03-08 17:59:44 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -21,5 +21,10 @@ bot.on('message', message => {
               message.channel.send('Roger le tavernier... Ça en fait des anagrammes ! Le rare goret verni. Le rire va te ronger. Roi, la verge rentre !')
               }
             })
+bot.on('message', message => {
+                            if (message.content === '!Déesse') {
+                            message.channel.send('Ma déesse adorée ? Tout petit devant toi, déesse Sundy…Je t en prie viens en moi, viens à moi…Je ne suis que ton oua…ille, ma foi croit en toi, tu incarnes la garce…LA GRACE…aide-moi… Je me tiens courbé, je suis tout petit mais ma foi grandi mais ma foi gran-dieeee…')
+                            }
+                          })
  
 bot.login('NDIxMDU4MTkyNTEzMTcxNDU3.DYLUrQ.gSin10CNlao1TPeM7kltokBYzjM')
