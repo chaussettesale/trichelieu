48a4595715c2e8fc9fef90d62fb56436c389fe7f
chaussettesale <37185103+chaussettesale@users.noreply.github.com>
2018-03-08 19:12:09 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -26,17 +26,17 @@ const Discord = require('discord.js')
                           })
       bot.on('message', message => {
                             if (message.content === '!Déesse') {
-                            message.channel.send('Ma déesse adorée ? Tout petit devant toi, déesse Sundy…Je t''en prie viens en moi, viens à moi…Je ne suis que ton oua…ille, ma foi croit en toi, tu incarnes la garce…LA GRACE…aide-moi… Je me tiens courbé, je suis tout petit mais ma foi grandi mais ma foi gran-dieeee…')
+                            message.channel.send('Ma déesse adorée ? Tout petit devant toi, déesse Sundy…Je t en prie viens en moi, viens à moi…Je ne suis que ton oua…ille, ma foi croit en toi, tu incarnes la garce…LA GRACE…aide-moi… Je me tiens courbé, je suis tout petit mais ma foi grandi mais ma foi gran-dieeee…')
                             }
                           })
       bot.on('message', message => {
                             if (message.content === '!chante') {
-                            message.channel.send('Pillons le seigneur mes frères et honorons ses sœurs ! Hosanna... hosanna... ose Anna me toucher plus bas... ose anna me tâter par là ! Allez lou ya... toi aussi vient là ! Mon cierge est allumé, la cire prête à couler, pour cette immaculée, c''est l''heure de communier...')
+                            message.channel.send('Pillons le seigneur mes frères et honorons ses sœurs ! Hosanna... hosanna... ose Anna me toucher plus bas... ose anna me tâter par là ! Allez lou ya... toi aussi vient là ! Mon cierge est allumé, la cire prête à couler, pour cette immaculée, c est l heure de communier...')
                             }
                           })
       bot.on('message', message => {
                             if (message.content === '!commands') {
-                            message.channel.send(' **ping**  **!tourdesentinelle** **!anagrammesroger** **!Déesse** **!chante**  ')
+                            message.channel.send(' **ping** \n **!tourdesentinelle** \n **!anagrammesroger** \n **!Déesse** \n **!chante**  ')
                              }
                              })
  bot.login('NDIxMDU4MTkyNTEzMTcxNDU3.DYMNAQ.9eabdClNS_vN0DQxD0gfXE5hec8')
