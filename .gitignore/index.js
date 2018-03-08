706b602bb3d863dcfb583833a3f6563f3a6d198e
chaussettesale <37185103+chaussettesale@users.noreply.github.com>
2018-03-08 22:16:04 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -35,8 +35,8 @@ const Discord = require('discord.js')
                             }
                           })
       bot.on('message', message => {
-                            if (message.content === '!commands') {
-                            message.channel.send(' Pour me controler il faut taper une des commandes suivantes:\n\n **__Catégorie Stratégie:__**\n\n ```!tourdesentinelle ```\n\n **__Catégorie Fun:__**\n\n ```ping``` \n\n **__Catégorie Tricheliade:__**\n\n ```!anagrammesroger``` \n ```!Déesse``` \n ```!chante ``` ')
+                            if (message.content === '!helps') {
+                            message.channel.send(' Pour me controler il faut taper une des commandes suivantes:\n\n **__Catégorie Stratégie:__**\n\n ```!tourdesentinelle ```\n\n **__Catégorie Fun:__**\n\n ```ping``` \n\n **__Catégorie Tricheliade:__**\n\n ```!anagrammesroger \n !Déesse \n !chante ``` ')
                              }
                              })
  bot.login('NDIxMDU4MTkyNTEzMTcxNDU3.DYMNAQ.9eabdClNS_vN0DQxD0gfXE5hec8')
