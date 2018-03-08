11033a24be38f2e28bdd5642e85f94480beac594
chaussettesale <37185103+chaussettesale@users.noreply.github.com>
2018-03-08 22:38:24 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -39,4 +39,19 @@ const Discord = require('discord.js')
                             message.channel.send(' Pour me controler il faut taper l\'une des commandes suivantes:\n\n **__Catégorie Stratégie:__**\n\n ```!tourdesentinelle ```\n\n **__Catégorie Fun:__**\n\n ```ping``` \n\n **__Catégorie Tricheliade:__**\n\n ```!anagrammesroger \n!déesse \n!chante ``` ')
                              }
                              })
+      bot.on('message', message => {
+                            if (message.content === '!HelloTrichelieu') {
+                                     if($(member) == "Chaussette[Words Master]"){
+                                      message.channel.send("Maitre!")
+                                      }
+                                      else if($(member) == "sundy")
+                                      {
+                                      message.channel.send("Maitresse!")
+                                      }
+                                      else
+                                      {
+                                      message.channel.send("salut toi.")
+                                      }
+                                      }
+                                      })
  bot.login('NDIxMDU4MTkyNTEzMTcxNDU3.DYMNAQ.9eabdClNS_vN0DQxD0gfXE5hec8')
